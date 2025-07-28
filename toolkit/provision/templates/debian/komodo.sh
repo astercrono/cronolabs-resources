599f65a0f80e90d98ad7165a1fff22da43599e00
Crono <crono@cronolabs.net>
2025-07-27 21:47:03 -0400

    Initial file setup

---
+++ toolkit/provision/templates/debian/komodo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+wget -P komodo https://raw.githubusercontent.com/moghtech/komodo/main/compose/mongo.compose.yaml &&
+	wget -P komodo https://raw.githubusercontent.com/moghtech/komodo/main/compose/compose.env
+
+# TODO: Everything else
---
+++ toolkit/provision/templates/fedora/workstation.sh
@@ -0,0 +1,42 @@
+# -- DNF Install:
+# fastfetch
+# fd-find
+# ffmpeg
+# flatpak
+# ghostty
+# gnome-tweaks
+# jetbrains-mono-fonts
+# librewolf
+# ripgrep
+# ruby
+# vlc
+# wezterm
+# xmms2
+# yt-dlp
+# lxc
+# lxc-templates
+# dnsmasq
+# :steam
+
+# -- Flatpak Install:
+# com.bitwarden.desktop
+# com.discordapp.Discord
+# fedora com.github.hugolabe.Wike
+# com.github.iwalton3.jellyfin-media-player
+# com.jgraph.drawio.desktop
+# com.mattjakeman.ExtensionManager
+# com.slack.Slack
+# com.usebruno.Bruno
+# dog.unix.cantata.Cantata
+# flathub com.transmissionbt.Transmission
+# flathub org.gimp.GIMP
+# flathub io.gitlab.theevilskeleton.Upscaler
+# flathub org.gnome.gitlab.YaLTeR.VideoTrimmer
+# im.riot.Riot
+# org.jitsi.jitsi-meet
+# org.libretro.RetroArch
+# org.jitsi.jitsi-meet
+# org.signal.Signal
+# com.protonvpn.www
+# io.dbeaver.DBeaverCommunity
+# :flathub com.obsproject.Studio
